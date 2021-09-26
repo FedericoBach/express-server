aefa646b8ba46282fd6dfc53cfb0610db438d934
LeandroBustos <LeandroBustos@github.com>
2021-09-25 23:08:40 -0300

    update: preparando servidor para handlebars

---
+++ routes/autorsRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get("/", (req, res) => res.send("Obtencion de autores."))
+
+router.get("/:id", (req, res) => res.send(`Obtencion de autores ${req.params.id}.`))
+
+router.post("/create", (req, res) => res.send("Creacion de autores."))
+
+router.post("/create/:id", (req, res) => res.send(`Creacion de autores ${req.params.id}.`))
+
+router.put("/update", (req, res) => res.send("Actualizacion de autores."))
+
+router.put("/update/:id", (req, res) => res.send(`Actualizacion de autores ${req.params.id}.`))
+
+router.delete("/delete", (req, res) => res.send("Eliminacion de autores."))
+
+router.delete("/delete/:id", (req, res) => res.send(`Eliminacion de autores ${req.params.id}.`))
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
 const booksRoutes = require('./routes/booksRoutes')
+const autorsRoutes = require('./routes/autorsRoutes')
 
 const app = express()
-
-app.use("/static", express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => res.send("hola mundo"))
 
 app.use("/books", booksRoutes)
+app.use("/autors", autorsRoutes)
 
 app.listen(5000, () => console.log("Servidor levantado en el puerto", 5000))
